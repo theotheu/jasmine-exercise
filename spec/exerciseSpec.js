16926eb7ea2a2d24f8bcfbb06c2abf13b5a18d7b
T Theunissen <theo.theunissen@gmail.com>
2014-02-12 09:32:40 +0100

    extra tests

--- spec/exerciseSpec.js
+++ spec/exerciseSpec.js
@@ -9,6 +9,23 @@ describe("Testing for side effects", function () {
         var retVal = testObj.attribute1;
         expect(retVal).toEqual(1);
     });
+    it("Test for object, attribute 2", function () {
+        var retVal = testObj.attribute2;
+        expect(retVal).not.toEqual(1);
+    });
+    it("Test for object, attribute 2", function () {
+        var retVal = testObj.attribute2;
+        expect(retVal).toEqual("1");
+    });
+    it("Test for object, length of attribute 3", function () {
+        var retVal = testObj.attribute3.length;
+        expect(retVal).toEqual(3);
+    });
+    it("Test for object, result of attribute 4", function () {
+        var retVal = testObj.attribute4();
+        expect(retVal).toEqual(true);
+    });
+
     it("Test for alert", function () {
         spyOn(window, "alert");
         sayHi();
