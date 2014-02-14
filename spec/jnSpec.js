f6e57b9b786be012241cb7de75b7cb9f3779eb2e
Theo Theunissen <theotheu@server7.tezzt.nl>
2014-02-14 21:40:35 +0100

    working jasmine-node test

---
+++ spec/jnSpec.js
@@ -0,0 +1,26 @@
+describe ("first test suite", function(){
+    it ("2 should equal 2", function(){
+        expect("2").toBe("2");
+    });
+    it ("1 should not equal 2", function(){
+        expect("1").not.toBe("2");
+    });
+})
+
+var request = require('request');
+
+describe("A suite", function() {
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
+
+describe("Something", function () {
+    it("Response should contain 'Hello and welcome!'", function (done) {
+        request("http://server7.tezzt.nl/~theotheu/jasmine-exercise/", function (error, response, body) {
+            expect(body).toContain("Hello and welcome!");
+            done();
+        });
+    });
+})
+
